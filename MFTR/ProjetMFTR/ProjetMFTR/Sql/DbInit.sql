825c77f2c7252d1a61e6eec553e2b9cc6a7fc1fd
Vincent Gélinas <gelinas.vincent@gmail.com>
2018-05-06 20:44:07 -0400

    Script Bd table suivi et intervenant

---
+++ MFTR/ProjetMFTR/ProjetMFTR/Sql/DbInit.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE Intervenant (
+    intervenant_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    nom nvarchar(50) NOT NULL,
+	prenom nvarchar(50) NOT NULL,
+	actif int Default 0
+);
+
+CREATE TABLE Suivi (
+    suivi_id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    dateSuivi date NOT NULL,
+    moment nvarchar(150),
+	remarque nvarchar(MAX),
+	enfant_id int NOT NULL,
+	dossier_id nvarchar(150) NOT NULL,
+	intervenant_id int NOT NULL,
+	Foreign Key (intervenant_id) References intervenant(intervenant_id),
+	Foreign Key (enfant_id) References enfants(enfant_id),
+	Foreign Key (dossier_id) References dossier(dossier_id)
+);
